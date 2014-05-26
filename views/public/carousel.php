0eb733e69057851028d54a3c5e45ecd45c8f6b90
patrickmj <patrickmjchnm@gmail.com>
2014-05-26 18:23:42 -0400

    put in showing titles. need to add the arg to choose, and add link on the title

--- views/public/carousel.php
+++ views/public/carousel.php
@@ -3,6 +3,9 @@
         <ul>
         <?php foreach($items as $item): ?>
             <li>
+            <p>
+            <?php echo metadata($item, array('Dublin Core', 'Title')); ?>
+            </p>
             <?php echo link_to_item(
                     item_image('square_thumbnail', array(), 0, $item), 
                     array('class' => 'image'), 'show', $item
