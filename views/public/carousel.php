3ec5415c93cbffa89b4bfcf6b334276f5411e24c
DBinaghi <39097246+DBinaghi@users.noreply.github.com>
2021-05-07 14:37:31 +0200

    Added new options and changed chevrons
    Added new options:
    - chance to use Item's title as tooltip for the images;
    - chance to hide the pagination controls;
    - chance to pause on hover and resume on mouse leave.
    
    Used FontAwesome's chevrons for previous and next buttons

--- views/public/carousel.php
+++ views/public/carousel.php
@@ -1,40 +1,55 @@
 <div class="jcarousel-wrapper">
     <div class="jcarousel" id="jcarousel-<?php echo $id_suffix; ?>">
         <ul>
-        <?php foreach($items as $item): ?>
+        <?php foreach ($items as $item): ?>
             <li>
-            
-            <?php echo link_to_item(
-                    item_image('square_thumbnail', array(), 0, $item), 
-                    array('class' => 'shortcode-carousel-image'), 'show', $item
-                    );
+            <?php 
+				$options = array();
+				if (isset($configs['carousel']['showTitlesAsTooltips']) && $configs['carousel']['showTitlesAsTooltips']) { 
+					$options['title'] = trim(strip_tags(metadata($item, array('Dublin Core', 'Title'))));
+				}
+			?>
+            <?php
+            echo link_to_item(
+                item_image('square_thumbnail', $options, 0, $item),
+                array('class' => 'shortcode-carousel-image'), 'show', $item
+            );
             ?>
-            <?php if(isset($configs['carousel']['showTitles']) && $configs['carousel']['showTitles'] ): ?>
+            <?php if (isset($configs['carousel']['showTitles']) && $configs['carousel']['showTitles']): ?>
                 <p class="shortcode-carousel-title">
-                <a href="<?php echo record_url($item, 'show'); ?>">
-                <?php echo metadata($item, array('Dublin Core', 'Title')); ?>
-                </a>
+					<a href="<?php echo record_url($item, 'show'); ?>">
+					<?php echo metadata($item, array('Dublin Core', 'Title')); ?>
+					</a>
                 </p>
             <?php endif; ?>
-            </li>
+           </li>
         <?php endforeach; ?>
         </ul>
     </div>
 
-    <a href="#" class="jcarousel-control-prev">&lsaquo;</a>
-    <a href="#" class="jcarousel-control-next">&rsaquo;</a>
+    <a href="#" class="jcarousel-controls jcarousel-control-prev"><span class="fa-stack" style="width: 1em"><i class="fas fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-left fa-stack-1x fa-inverse"></i></span></a>
+    <a href="#" class="jcarousel-controls jcarousel-control-next"><span class="fa-stack" style="width: 1em"><i class="fas fa-circle fa-stack-1x"></i><i class="fa fa-chevron-circle-right fa-stack-1x fa-inverse"></i></span></a>
 
-    <p class="jcarousel-pagination"></p>
+    <?php if (!isset($configs['carousel']['hidePagination']) || !$configs['carousel']['hidePagination']): ?>
+	<p class="jcarousel-pagination"></p>
+	<?php endif; ?>
 </div>
 
 <script type='text/javascript'>
-jQuery(function() {
-    var carouselConfig = <?php echo json_encode($configs['carousel']);?>;
-    var configs = <?php echo json_encode($configs);?>;
-    var carousel = jQuery('#jcarousel-<?php echo $id_suffix; ?>').jcarousel(carouselConfig);
-    <?php if(isset($configs['autoscroll'])): ?>
-    var autoscrollConfig = <?php echo json_encode($configs['autoscroll']);?>;
-    carousel.jcarouselAutoscroll(autoscrollConfig);
-    <?php endif; ?>
-});
+	jQuery(function() {
+		var carouselConfig = <?php echo json_encode($configs['carousel']); ?>;
+		var configs = <?php echo json_encode($configs); ?>;
+		var carousel = jQuery('#jcarousel-<?php echo $id_suffix; ?>').jcarousel(carouselConfig);
+		<?php if (isset($configs['autoscroll'])): ?>
+		var autoscrollConfig = <?php echo json_encode($configs['autoscroll']); ?>;
+		carousel.jcarouselAutoscroll(autoscrollConfig)
+		<?php if (isset($configs['autoscroll']['pauseOnHover']) && $configs['autoscroll']['pauseOnHover']): ?>
+		.hover(function() {
+			jQuery(this).jcarouselAutoscroll('stop');
+		}, function() {
+			jQuery(this).jcarouselAutoscroll('start');
+		});
+		<?php endif; ?>
+		<?php endif; ?>
+	});
 </script>
